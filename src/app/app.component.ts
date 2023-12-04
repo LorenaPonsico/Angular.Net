458bf587b2c544b159c80bba7beb942f096910b3
Lorena Ponsico <lorena.poca@gmail.com>
2023-12-04 10:25:25 +0100

    Project start with 3 components and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Frontend';
+  nombre = "Lorena";
+
+  constructor() {
+    setInterval(() => this.nombre = "Javier", 3000);
+    
+    
+  }
 }
